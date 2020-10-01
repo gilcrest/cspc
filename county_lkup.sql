70220cece3180a7035ede73b2dca08b3160df09a
Dan Gillis <gilcrest@gmail.com>
2020-09-30 22:58:02 -0400

    take out bad unique index

--- county_lkup.sql
+++ county_lkup.sql
@@ -23,14 +23,11 @@ comment on column lookup.county_lkup.county_id is 'Arbitrary unique ID for prima
 comment on column lookup.county_lkup.country_alpha_2_cd is 'ISO 3166-1 Alpha 2 Code';
 comment on column lookup.county_lkup.state_prov_cd is 'The state or province code for the region of the country';
 comment on column lookup.county_lkup.county_cd is 'A unique code for the county. For the US, this are Federal Information Processing System (FIPS) Codes for States and Counties. FIPS codes are numbers which uniquely identify geographic areas.';
+comment on column lookup.county_lkup.county_fips_cd is 'US Only. FIPS codes are numbers which uniquely identify geographic areas.  The number of digits in FIPS codes vary depending on the level of geography.  State-level FIPS codes have two digits, county-level FIPS codes have five digits of which the first two are the FIPS code of the state to which the county belongs.';
 comment on column lookup.county_lkup.latitude_average is 'Average latitude of the county territory';
 comment on column lookup.county_lkup.longitude_average is 'Average longitude of the state or province territory';
-comment on column lookup.county_lkup.county_fips_cd is 'US Only. FIPS codes are numbers which uniquely identify geographic areas.  The number of digits in FIPS codes vary depending on the level of geography.  State-level FIPS codes have two digits, county-level FIPS codes have five digits of which the first two are the FIPS code of the state to which the county belongs.';
 
 alter table lookup.county_lkup owner to postgres;
-
-create unique index county_lkup_ui1
-    on lookup.county_lkup (country_alpha_2_cd, state_prov_cd);
 
 create unique index county_lkup_ui2
     on lookup.county_lkup (county_cd);
