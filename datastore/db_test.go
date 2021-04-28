b1652a69586facd8e0dbc0bfd260529ecce9a28e
Dan Gillis <gilcrest@gmail.com>
2021-04-28 17:33:19 -0400

    change to datastore pattern

--- datastore/db_test.go
+++ datastore/db_test.go
@@ -4,36 +4,42 @@ import (
 	"os"
 	"testing"
 
+	"github.com/gilcrest/cspc/logger"
+
 	"github.com/rs/zerolog"
 )
 
-func Test_NewLocalDB(t *testing.T) {
+func Test_NewDB(t *testing.T) {
 	type args struct {
-		n Name
+		pgds PGDatasourceName
+		l    zerolog.Logger
 	}
-	zerolog.TimeFieldFormat = ""
 
-	// set logging level based on input
-	zerolog.SetGlobalLevel(zerolog.DebugLevel)
-
-	// start a new logger with Stdout as the target
-	logger := zerolog.New(os.Stdout).With().Timestamp().Logger()
+	lgr := logger.NewLogger(os.Stdout, zerolog.DebugLevel, true)
+	dsn := NewPGDatasourceName("localhost", "go_api_basic", "postgres", "", 5432)
+	baddsn := NewPGDatasourceName("badhost", "go_api_basic", "postgres", "", 5432)
 
 	tests := []struct {
-		name string
-		args args
+		name    string
+		args    args
+		wantErr bool
 	}{
-		{"App DB", args{LocalDatastore}},
+		{"App DB", args{dsn, lgr}, false},
+		{"Bad DSN", args{baddsn, lgr}, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			db, _, err := NewDB(tt.args.n, logger)
-			if err != nil {
-				t.Errorf("Error from newDB = %v", err)
+			db, cleanup, err := NewDB(tt.args.pgds, tt.args.l)
+			t.Cleanup(cleanup)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewDB() error = %v, wantErr %v", err, tt.wantErr)
+				return
 			}
-			err = db.Ping()
-			if err != nil {
-				t.Errorf("Error pinging database = %v", err)
+			if err == nil {
+				err = db.Ping()
+				if err != nil {
+					t.Errorf("Error pinging database = %v", err)
+				}
 			}
 		})
 	}
